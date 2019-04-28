280544b6b304391ac2c14789b4c845495a2968ac
Marius Kuprys <mariuskuprys@Mariuss-MacBook-Pro.local>
2019-04-28 11:22:20 +0300

    new java file

---
+++ NewJavaFile.java
@@ -0,0 +1,45 @@
+public class NewjavaFile {
+    public static void main(String[] args) {
+        //System.out.println("Hello World");
+        // variable name = value ;
+
+       /* int myNumber = 10;
+        System.out.println(myNumber);
+
+        myNumber = 11;
+        System.out.println(myNumber);
+
+        myNumber = 12;
+        System.out.println(myNumber);
+
+        myNumber = 13;
+        System.out.println(myNumber);
+
+        myNumber = 14;
+        System.out.println(myNumber);
+
+        myNumber = 20;
+        System.out.println(myNumber);*/
+
+       float a, b, c, d;
+       a=10;
+       b=20;
+
+
+       c=a+b;
+       d=a/b;
+
+        System.out.println(d);
+        // short small number 16bits;
+        //byte 8bits;
+        //int 32bit;
+        //long 64bit;
+
+        //float averageDecimal 32bit1.2334455566;
+        //double largeDecimal 64bit;
+
+        //boolean trueOrFalse;
+        //char character = anyCharacter 'a' 16bit
+
+
+    }
